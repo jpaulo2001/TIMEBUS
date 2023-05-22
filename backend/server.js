61ed9a95ec451505e51e4a00865b043a87a07ab1
João Silva <joaopaulo6069@hotmail.com>
2023-05-22 12:46:18 +0000

    connection to db done

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+
 
 
 
@@ -19,8 +21,18 @@ app.use((req, res, next)=>{
 //Routes
 app.use('/api/workouts', workoutRoutes)
 
-// listen for request
-app.listen(process.env.PORT, () =>{
-    console.log("Listening on Porte", process.env.PORT)
-})
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for request
+        app.listen(process.env.PORT, () =>{
+            console.log("Connected to db", process.env.PORT)
+        })
+    })
+    .catch((Error) => {
+        console.log(Error)
+    })
+
+
 
