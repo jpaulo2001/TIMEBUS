ffd0dc728851beac636cfed8537f0a36d0012bbd
João Silva <joaopaulo6069@hotmail.com>
2023-05-22 16:56:09 +0000

    models
    models

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -14,8 +15,15 @@ router.get('/:id', (req,res) => {
 })
 
 //post a new workout
-router.post('/', (req,res) => {
-    res.json({msg:'post a new workout'})
+router.post('/', async (req,res) => {
+    const {title,load,reps} = req.body // extract the data
+    try {
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    }
+    catch(Error){
+        res.status(400).json({error: error.message})
+    }
 })
 
 
