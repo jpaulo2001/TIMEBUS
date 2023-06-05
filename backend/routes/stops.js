81ef8cccd0ee23bce848e192c9e43a6f1d237609
Gheorghiu Alin <aggheorghiu@hotmail.com>
2023-06-05 22:30:47 +0300

    get stops in bEnd

--- backend/routes/stops.js
+++ backend/routes/stops.js
@@ -2,18 +2,11 @@ const express = require('express')
 const {
   getStopByName,
   getStops,
-  searchBuses,
-  searchStops,
-  updateStop,
-  deleteStop
 } = require('../controllers/busStopController.js')
 
 const router = express.Router()
 
 router.get('/:stopName', getStopByName);
 router.get('/', getStops);
-// router.get('/buses/:id', searchBuses)
-// router.put('/:id', updateStop);
-// router.delete('/:id', deleteStop);
 
 module.exports = router
--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,105 +1,99 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const WorkoutForm = () => {
   const [searchResults, setSearchResults] = useState([]);
-
-  const [name, setTitle] = useState('');
-  const [load, setLoad] = useState('');
-  const [reps, setReps] = useState('');
+  const [name, setName] = useState('');
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [emptyFields, setEmptyFields] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     if (!name) {
       setError('Name is required');
       return;
     }
-  
+
+    setLoading(true);
+    setSearchResults([]);
+    setError(null);
+
     try {
       const encodedName = encodeURIComponent(name);
-      const response = await fetch(`http://localhost:4000/api/stops/search/${encodedName}`);
-  
-      const data = await response.json(); // Parse the response data
-  
+      const response = await axios.get(`http://localhost:4000/api/stops/${encodedName}`);
+      const data = response.data;
+
       if (Array.isArray(data) && data.length > 0) {
         setSearchResults(data);
-        setError(null);
-        setEmptyFields([]);
       } else {
-        setSearchResults([]);
         setError('No matching stops found.');
-        setEmptyFields([]);
       }
     } catch (error) {
       console.error('Error searching for stops:', error);
-      setSearchResults([]);
-      setError('An error occurred while searching for stops.');
-      setEmptyFields([]);
+      if (error.response) {
+        setError('An error occurred while searching for stops: ' + error.response.data.error);
+      } else if (error.request) {
+        setError('Network error occurred. Please try again later.');
+      } else {
+        setError('An unexpected error occurred. Please try again.');
+      }
+    } finally {
+      setLoading(false);
     }
   };
-  
-  
 
 
-const style = {
-  position: 'relative',
-  marginTop: '10%',
-  margin: '5%',
-  background: 'white',
-  border: '50px white solid',
-  borderRadius: '30px',
+
+
+  const style = {
+    position: 'relative',
+    marginTop: '10%',
+    margin: '5%',
+    background: 'white',
+    border: '50px white solid',
+    borderRadius: '30px',
+  };
+
+  return (
+    <form className="create" style={style} onSubmit={handleSubmit}>
+      <h3>MAKE YOUR SEARCH</h3>
+
+      <label>Name:</label>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+
+      <button type="submit" onClick={handleSubmit}>
+        Search Bus
+      </button>
+
+      {loading && <div>Loading...</div>}
+
+      {error && <div className="error">{error}</div>}
+
+      {searchResults.length > 0 && (
+        <div>
+          <h4>Search Results:</h4>
+          <ul>
+            {searchResults.map((bus) => (
+              <li key={bus.id}>
+                <strong>Name:</strong> {bus.name}
+                <br />
+                <strong>Route:</strong> {bus.route}
+                <br />
+                {/* Display other bus details as needed */}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </form>
+  );
 };
 
-return (
-  <form className="create" style={style} onSubmit={handleSubmit}>
-    <h3>MAKE YOUR SEARCH</h3>
 
-    <label>Name:</label>
-    <input
-      type="text"
-      onChange={(e) => setTitle(e.target.value)}
-      value={name}
-      className={emptyFields.includes('title') ? 'error' : ''}
-    />
-
-    <label>Location A:</label>
-    <input
-      type="string"
-      onChange={(e) => setLoad(e.target.value)}
-      value={load}
-      className={emptyFields.includes('load') ? 'error' : ''}
-    />
-
-    <label>Location B</label>
-    <input
-      type="string"
-      onChange={(e) => setReps(e.target.value)}
-      value={reps}
-      className={emptyFields.includes('reps') ? 'error' : ''}
-    />
-
-    <button>Search Bus</button>
-    {error && <div className="error">{error}</div>}
-    {searchResults.length > 0 && (
-      <div>
-        <h4>Search Results:</h4>
-        <ul>
-          {searchResults.map((bus) => (
-            <li key={bus.id}>
-              <strong>Name:</strong> {bus.name}
-              <br />
-              <strong>Route:</strong> {bus.route}
-              <br />
-              {/* Display other bus details as needed */}
-            </li>
-          ))}
-        </ul>
-      </div>
-    )}
-  </form>
-);
-};
 
 export default WorkoutForm;
