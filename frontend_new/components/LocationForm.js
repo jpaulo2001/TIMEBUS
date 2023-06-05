f50e57610b2f56468fd874391149e2e46310da07
serralva-ruben <91135825+serralva-ruben@users.noreply.github.com>
2023-06-05 21:10:35 +0000

    suggestion box added, ip for backend added to .env of frontend

--- frontend_new/components/LocationForm.js
+++ frontend_new/components/LocationForm.js
@@ -1,22 +1,109 @@
-import { Image, StyleSheet, TextInput, View, Text, TouchableOpacity, Dimensions} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { REACT_APP_BACKEND_IP } from '@env'
+import { Image, StyleSheet, TextInput, View, Text, TouchableOpacity, Dimensions, SafeAreaView, FlatList } from 'react-native';
 
 export default function LocationForm() {
-  
+  const [filterDataA, setfilterDataA] = useState([]);
+  const [masterDataA, setmasterDataA] = useState([]);
+  const [searchA, setSearchA] = useState('');
 
+  const [filterDataB, setfilterDataB] = useState([]);
+  const [masterDataB, setmasterDataB] = useState([]);
+  const [searchB, setSearchB] = useState('');
+
+  useEffect(()=>{
+    fetchStops(setfilterDataA, setmasterDataA);
+    fetchStops(setfilterDataB, setmasterDataB);
+    return() => {}
+  }, [])
+
+
+  const fetchStops = (setFilterData,setMasterData) => {
+    const apiURL = `http://${REACT_APP_BACKEND_IP}:4000/api/stops/`;
+    fetch(apiURL)
+    .then((response)=>response.json())
+    .then((responseJson) => {
+      setFilterData(responseJson);
+      setMasterData(responseJson);
+    }).catch((error)=>{
+      console.error(error);
+    })
+  }
+
+  const searchFilter = (text, setFilterData, masterData, setSearch) => {
+    if(text){
+      const newData = masterData.filter((item) =>{
+        const itemData = item.stopName ? item.stopName.toUpperCase() : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilterData(newData);
+      setSearch(text);
+    }else{
+      setFilterData(masterData);
+      setSearch(text);
+    }
+  }
+
+  const ItemView = ({item}) => {
+    return(
+      <View style={styles.itemStyle}>
+        <Text style={styles.suggestionBoxTypography}>
+          {item.stopName}{' => lat: '}{item.lat.toFixed(2)}{', lng: '}{item.lng.toFixed(2)}
+        </Text>
+      </View> 
+    )
+  }
+
+  const ItemSeparatorView = () => {
+    return(
+      <View
+        style={{height: 0.5, width: '100%', backgroundColor: 'black'}}
+      />
+    )
+  }
+  
   return (
     <View style={styles.container}>
         <View style={styles.textInputConainer}>
           <Text style={styles.stopLabel}>Stop A</Text>
-            <TextInput style={styles.textInput}></TextInput>
+            <TextInput 
+              style={styles.textInput}
+              value={searchA}
+              onChangeText={(text) => searchFilter(text, setfilterDataA, masterDataA, setSearchA)}
+            />
+            {searchA&&filterDataA.length>0 ? (
+              <View style={styles.suggestionBox}>
+                <FlatList
+                  data={filterDataA}
+                  keyExtractor={(item, index) => index.toString()}
+                  ItemSeparatorComponent={ItemSeparatorView}
+                  renderItem = {ItemView}
+                />
+              </View>
+            ):null}
           <Text style={styles.stopLabel}>Stop B</Text>
-            <TextInput style={styles.textInput}></TextInput>
+            <TextInput 
+              style={styles.textInput}
+              value={searchB}
+              onChangeText={(text) => searchFilter(text, setfilterDataB, masterDataB, setSearchB)}
+            />
+            {searchB&&filterDataB.length>0 ? (
+              <View style={styles.suggestionBox}>
+                <FlatList
+                  data={filterDataB}
+                  keyExtractor={(item, index) => index.toString()}
+                  ItemSeparatorComponent={ItemSeparatorView}
+                  renderItem = {ItemView}
+                />
+              </View>
+            ):null}
         </View>
           
         <TouchableOpacity style= {styles.searchButton}>
           <Image source={require('../public/assets/buttons/search.png')} style={styles.searchButtonImage}/>
         </TouchableOpacity>
     </View>
-
   );
 }
 
@@ -68,5 +155,20 @@ const styles = StyleSheet.create({
     stopLabel: {
       height: windowHeight*0.02,
       width: windowWidth*0.2,
+    },
+
+    suggestionBox:{
+      borderColor: 'black',
+      borderWidth: '2px',
+      left: windowWidth*0.05,
+      top: -windowHeight*0.15,
+      backgroundColor: 'grey',
+      position: 'absolute',
+    },
+    suggestionBoxTypography:{
+      fontSize:13,
+    },
+    itemStyle:{
+      maxHeight:windowHeight*0.03,
     }
 });
