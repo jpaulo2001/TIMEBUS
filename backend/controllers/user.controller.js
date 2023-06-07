700d0c28731bd132ba969810beccafc91167739f
João Silva <joaopaulo6069@hotmail.com>
2023-06-07 16:03:30 +0000

    fixes controllers

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,10 @@
 const User = require("../models/user.model.js");
 const createError = ("../utils/createError.js");
 
+
+
+
+
 const deleteUser = async (req, res, next) => {
   const user = await User.findById(req.params.id);
 
