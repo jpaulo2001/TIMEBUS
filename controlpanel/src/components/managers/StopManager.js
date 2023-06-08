391c8cab043a1acdf5980eb0c324c5aedfeb326a
Gheorghiu Alin <aggheorghiu@hotmail.com>
2023-06-08 06:22:09 +0300

    .

---
+++ controlpanel/src/components/managers/StopManager.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom'
+
+function StopManager() {
+
+  const [stops, setStops] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/stops')
+      .then((res) => res.json())
+      .then((data) => setStops(data))
+      .catch((err) => console.log(err));
+    }, []);
+
+  return (
+      <div style={styles.formContainer}>
+        <ul style={styles.elementsList}>
+            {stops.map((stop, index)=>(
+              <div>
+                <li style={styles.Typography}>Name: {stop.stopName} , lat: {stop.lat} , lng: {stop.lng}</li>
+                <hr style={styles.separatorItem}/>
+              </div>
+            ))}
+        </ul>
+        <Link to="/Dashboard/StopForm" style={styles.addTypography}>Add Stop</Link>
+      </div>
+  );
+}
+
+export default StopManager
+
+const styles = {
+  formContainer:{
+    margin: '5%',
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: '2px',
+    borderStyle: 'dashed',
+    borderRadius:'30px',
+    backgroundColor: '#8ab8a8',
+    padding: '5%',
+  },
+  Typography:{
+    fontSize: '2rem',
+    fontFamily: 'American Typewriter',
+  },
+  addTypography:{
+    backgroundColor: 'Green',
+    borderRadius: '25px',
+    marginTop: '10vh',
+    padding: '20px',
+  },
+  addButton:{
+    height: '50px',
+    width: '100px',
+    margin: '20px',
+    backgroundColor: '#c6cca5',
+    borderRadius: '15px',
+    border: 'solid',
+    borderWidth: '1px',
+    cursor: 'pointer',
+    fontSize: '80%',
+    fontFamily: 'American Typewriter',
+  },
+  inputContainer:{
+    height: '100vh',
+  },
+  inputField: {
+    padding: '5px',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+    fontSize: '100%',
+    fontFamily: 'American Typewriter',
+  },
+  formGroup: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: '20px',
+  },
+  elementsList: {
+    display: 'block',
+    listStyle: 'none',
+    background: 'grey',
+    borderRadius: '20px',
+    padding: '30px',
+    width: '70vw'
+  },
+  separatorItem: {
+    backgroundColor: 'Transparent',
+
+  }
+}
