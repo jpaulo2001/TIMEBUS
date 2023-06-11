601dcc6dd6c72d5dba562c2a4f0c67ea55270c33
serralva-ruben <91135825+serralva-ruben@users.noreply.github.com>
2023-06-11 18:10:34 +0000

    login is coming

--- frontend/App.js
+++ frontend/App.js
@@ -3,9 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import MapContainer from './components/MapContainer';
 import LocationForm from './components/LocationForm';
 import Logo from './components/Logo';
+import { useState } from 'react';
+import LoginScreen from './components/Login';
+import HomeScreen from './components/HomeScreen';
 
 
 export default function App() {
+  const [stops, setStops] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <View style={styles.container}>
       {isLoggedIn ? (<HomeScreen/>) : (<LoginScreen/>)}
@@ -16,5 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,21 +1,23 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, View, TextInput, Button, Alert, Dimensions } from 'react-native';
 import axios from 'axios';
 
-export default function LoginScreen({ onLogin }){
+export default function LoginScreen(){
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = async () => {
     try {
-      onLogin(username, password);
-
       const response = await fetch('http://localhost:4000/api/auth/login', {
-        username,
-        password,
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 
+            "username":username,
+            "password":password 
+        }),
       });
 
-      const { token } = response.data;
+      const token  = response.json()
 
       // Store the token securely (e.g., using AsyncStorage or a secure storage mechanism)
 
@@ -38,21 +40,38 @@ export default function LoginScreen({ onLogin }){
         placeholder="Username/Email"
         value={username}
         onChangeText={(text) => setUsername(text)}
+        style={styles.input}
       />
       <TextInput
         placeholder="Password"
         secureTextEntry
         value={password}
         onChangeText={(text) => setPassword(text)}
+        style={styles.input}
       />
       <Button title="Login" onPress={handleLogin} />
     </View>
   );
 };
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
+        flex: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
+    input: {
+        width: windowWidth*0.5,
+        height: 40,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 10,
+      },
+
   });
 
