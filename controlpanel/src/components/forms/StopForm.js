aa1e935c8bec889671c2e942a56e9cd15e7093b3
joaosilva <joaopaulo6069@hotmail.com>
2023-07-03 12:00:00 +0000

    +

--- controlpanel/src/components/forms/StopForm.js
+++ controlpanel/src/components/forms/StopForm.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect} from "react";
 import {Link, useNavigate} from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faArrowLeft, faThumbtack} from '@fortawesome/free-solid-svg-icons'
